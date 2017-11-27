d580bf7da4fe19d526195c92a62f1885b1573c1f
github username <github email address>
2017-11-27 15:58:15 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,9 @@ Rails.application.routes.draw do
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
+  #get 'posts/:id', to: 'posts#show'
+  resources :posts, only: :show
+
   # Example resource route with options:
   #   resources :products do
   #     member do
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+p1 = Post.create(title: "A post about my dog", description: "This is my post about my dog")
+p2 = Post.create(title: "Another post about my dog", description: "This is another my post about my dog")
+p3 = Post.create(title: "Yet Another post about my dog", description: "This is yet another my post about my dog")
